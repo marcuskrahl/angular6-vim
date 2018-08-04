ef4bd041adc6b2a789ac26654904f0eee9509ea8
Marcus Krahl <mail@marcuskrahl.de>
2018-08-04 11:00:36 +0200

    add sample code for typescript lsp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core"
+import { Router, ActivatedRoute } from "@angular/router"
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = 'app';
+  title: number | string = "app"
+
+  constructor(private router: Router, private activatedRoute: ActivatedRoute) {}
+
+  public test(): void {
+    console.log("this is a test")
+    this.test();
+    this.title = 42
+    test();
+    this.title = 42;
+  }
+
+  private function3(test: any[]): void {
+    this.router.navigate(["."])
+  }
+
+  private function2(n: number, t: string): boolean {
+    return n == 42 || t == "fourtytwo"
+  }
 }
---
+++ test.js
@@ -0,0 +1,15 @@
+function bla() {
+  return 42;
+}
+
+
+var test = {
+  a: 42,
+  b: {
+    c: 52
+  }
+};
+
+test
+
+
